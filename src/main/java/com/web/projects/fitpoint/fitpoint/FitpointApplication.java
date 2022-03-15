97311060503f55749362e536ff72c5d7a248db17
Мартин's PC <fidanovski.martin@gmail.com>
2022-03-15 15:31:06 +0100

    First Push 1

--- src/main/java/com/web/projects/fitpoint/fitpoint/FitpointApplication.java
+++ src/main/java/com/web/projects/fitpoint/fitpoint/FitpointApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@WebComponentScan
 public class FitpointApplication {
 
     public static void main(String[] args) {
