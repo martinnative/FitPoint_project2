6f3c3838b9e0648d6f38a310eb56ec3dbdf78672
Мартин's PC <fidanovski.martin@gmail.com>
2022-03-15 14:35:06 +0100

    First Push

---
+++ src/test/java/com/web/projects/fitpoint/fitpoint/FitpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.projects.fitpoint.fitpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitpointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
