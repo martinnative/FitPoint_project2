f30b6cf472589b15f53e179c1e0ccbc31e10a596
msvilanovik <msvilanovik@yahoo.com>
2022-03-15 15:35:22 +0100

    commit

--- src/main/java/com/web/projects/fitpoint/fitpoint/FitpointApplication.java
+++ src/main/java/com/web/projects/fitpoint/fitpoint/FitpointApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@WebComponentScan
 public class FitpointApplication {
 
     public static void main(String[] args) {
